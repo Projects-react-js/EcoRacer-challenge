1d73391c1561defa31249da0b03409337bad2f57
lorransantosdev <lorransantossarmentol@gmail.com>
2024-09-25 11:20:58 -0300

    Alteração na estilização do banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,8 +5,8 @@ export default function Banner({ children }){
             <section className="flex items-center justify-around flex-col relative
             w-full min-w-80 h-screen 
             bg-custom-radial 
-            lg:px-20 lg:py-40 lg:gap-5 
-            md:px-40 md:py-40 md:gap-5 
+            lg:px-20 lg:py-48 lg:gap-5 
+            md:px-40 md:py-48 md:gap-5 
             px-10 py-20"> 
                 {children}
             </section>
